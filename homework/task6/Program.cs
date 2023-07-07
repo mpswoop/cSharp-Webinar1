32e52794cbda15a9d2c3e6b331080a105f48b907
MP <mp@swoop.pro>
2023-07-07 19:04:43 +0300

    homework task 6 done

---
+++ homework/task6/Program.cs
@@ -0,0 +1,12 @@
+﻿//even number
+Console.WriteLine("Let's check if your number is even.");
+Console.WriteLine("Please input number:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Yes, it's even");
+}
+else
+{
+    Console.WriteLine("No, its odd");
+}
