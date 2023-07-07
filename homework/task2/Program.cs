9bb4de54ed49e133bb8a3b655391c69dfa4d367c
MP <mp@swoop.pro>
2023-07-07 18:23:06 +0300

    homework task  2 done

---
+++ homework/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// bigger number
+Console.WriteLine("Please input two numbers");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+Console.WriteLine(num2);
+}
