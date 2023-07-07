070ec06d7c50257ee6f5eb6886419ed59201d2e0
MP <mp@swoop.pro>
2023-07-07 17:21:56 +0300

    task 4 unsolved - error

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// input digit - show row form -digit to digit
+Console.WriteLine("please input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int output = number * (-1);
+if (output < 0)
+{
+    output == output * -1;
+}
+while (number > output)
+{
+    Console.WriteLine(output);
+    output++
+}
