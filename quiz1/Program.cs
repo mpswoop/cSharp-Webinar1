c662a2e562a4447905d12064368b017ff92c5267
MP <mp@swoop.pro>
2023-07-07 09:04:10 +0300

    task 1 number squared

---
+++ quiz1/Program.cs
@@ -0,0 +1,7 @@
+﻿int i = 0;
+
+while (i < 10)
+{
+    i += 2;
+    Console.WriteLine(i);
+}
---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// squared integer
+Console.Clear();
+Console.WriteLine("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+Console.WriteLine($"result: {number * number}");
+// Console.WriteLine(square);
+
