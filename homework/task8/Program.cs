3498ecf5be922553bcf9d4128b4c506038e68dd8
MP <mp@swoop.pro>
2023-07-07 19:23:02 +0300

    homework task 8 code done, but syntax error unsoved - HELPgit add .!

---
+++ homework/task8/Program.cs
@@ -0,0 +1,25 @@
+﻿//row of even numbers
+Console.WriteLine("Let's check if your number is even.");
+Console.WriteLine("Please input number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (num == 0)
+{
+    Console.WriteLine("0");
+}
+if (num > 0)
+{
+    while (num > i)
+    {
+    i == i+2;
+    Console.WriteLine(i);
+    }
+}
+else if (num < 0)
+{
+    while (num * -1 > i)
+    {
+    i == i+2;
+    Console.WriteLine(i * -1);
+    }
+}
